30dfafeb8fc01c3e33cf5cd181768d74a53435aa
Kevin Gates <kevinobamatheus@gmail.com>
2018-02-01 11:17:51 +0800

    the prices were undefined

--- sync.php
+++ sync.php
@@ -1,8 +1,5 @@
 <?php
 require __DIR__.'/predis/autoload.php';
-//redis-cli -h 54.238.155.92 -p 6379 -a r0b0deckPublicred1s95321 
-//54.238.128.48
-//54.238.155.92
 $singleServerMaster = array(
     'host' => 'localhost',
     'port' => 6380,
@@ -13,30 +10,21 @@ $singleServerMaster = array(
 $singleServerSlave = array(
     'host' => 'localhost',
     'port' => 6379,
-    'database' => 1,    
+    'database' => 0,    
 );
 
 $client = new Predis\Client($singleServerMaster);
 $clientSlave = new Predis\Client($singleServerSlave);
 
-$allKeys = $client->keys('*');
-//print_r($allKeys);
+$allKeys = array('API','ARB','ARBI','CONST','DEPTH','PRICES','TICKER');
+
 $clientSlave->flushdb();
-foreach ($allKeys as $index => $rediskey) {
-	 //echo($rediskey);
-	//if($index==1 || $index == 3) {
-	 if(strpos($rediskey,'laravel') === false) {
-	 	 $haskAll = $client->hgetall($rediskey);
-	 	foreach ($haskAll as $hashKey => $hashValue) {	 		
-	 		$result = $clientSlave->hset($rediskey,$hashKey, $hashValue);
-	 		if($result == 1) echo($rediskey.' '.$hashKey.' has been updated.');
-	 	}	 	
-	 	//echo('data='.$rediskey."   ");
-	 }		
-	//}	
+foreach ($allKeys as $index => $rediskey) {	 	 
+ 	 echo($rediskey.' ');
+ 	 $haskAll = $client->hgetall($rediskey);
+ 	foreach ($haskAll as $hashKey => $hashValue) {	 		
+ 		$result = $clientSlave->hset($rediskey,$hashKey, $hashValue);
+ 		//if($result == 1) echo($rediskey.' '.$hashKey.' has been updated.');
+ 	}	 		 		
 }
-
-//echo($return);
-
-
 ?>
